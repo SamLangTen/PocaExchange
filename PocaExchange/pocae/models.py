ef644ac9fa9656190dc9246d9c9981e78341796e
SamLangTen <samlangten@outlook.com>
2018-01-26 09:39:21 +0800

    test orm function

--- PocaExchange/pocae/models.py
+++ PocaExchange/pocae/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-STATE_CHOICES = ((1,'Sending'),(2,'Sent'),(3,'Received'))
+STATE_CHOICES = ((1, 'Sending'), (2, 'Sent'), (3, 'Received'))
+
 
 class PostcardPair(models.Model):
-    pair_id = models.UUIDField()
-    sender = models.CharField(max_length = 30)
-    receiver = models.CharField(max_length = 30)
+    pair_id = models.UUIDField(primary_key=True)
+    sender = models.CharField(max_length=30)
+    receiver = models.CharField(max_length=30)
     send_date = models.DateTimeField()
     receive_date = models.DateTimeField()
     state = models.IntegerField(choices=STATE_CHOICES)

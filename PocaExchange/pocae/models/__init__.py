cdd856d97d169b432777823d26c2c70e350cdf20
SamLangTen <samlangten@outlook.com>
2018-01-28 23:29:23 +0800

    add user group field

--- PocaExchange/pocae/models/__init__.py
+++ PocaExchange/pocae/models/__init__.py
@@ -1,4 +1,4 @@
-from .user import User
+from .user import User,USER_GROUP_TYPES
 from .postcard_pair import PostcardPair, STATE_CHOICES
 from .drift_bottle import DriftBottle
 
--- PocaExchange/pocae/models/user.py
+++ PocaExchange/pocae/models/user.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+USER_GROUP_TYPES = [(1, 'Administrator'), (2, 'Exchanger')]
+
+
 class User(models.Model):
     user_id = models.UUIDField(primary_key=True)
     user_name = models.CharField(max_length=100)
     password = models.TextField()
+    user_group = models.IntegerField(choices=USER_GROUP_TYPES,default=2)

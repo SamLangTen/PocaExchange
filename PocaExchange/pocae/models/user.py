1663293f43c76b750b974c3b6c5c3f6ba8c9e1b5
SamLangTen <samlangten@outlook.com>
2018-01-29 11:37:02 +0800

    add user serializer prepare to split dto and dm

--- PocaExchange/pocae/models/user.py
+++ PocaExchange/pocae/models/user.py
@@ -6,5 +6,5 @@ USER_GROUP_TYPES = [(1, 'Administrator'), (2, 'Exchanger')]
 class User(models.Model):
     user_id = models.UUIDField(primary_key=True)
     user_name = models.CharField(max_length=100)
-    password = models.TextField()
+    password = models.CharField(max_length=100)
     user_group = models.IntegerField(choices=USER_GROUP_TYPES, default=2)
---
+++ PocaExchange/pocae/serializers/user_serializer.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from pocae.models import *
+import uuid
+import hashlib
+
+class UserCreationSerializer(serializers.Serializer):
+    user_id = serializers.UUIDField(required=False)
+    user_name = serializers.CharField(max_lenth=100)
+    password = serializers.CharField(max_lenth=100)
+    user_group = serializers.ChoiceField(choices=USER_GROUP_TYPES)
+
+    def create(self, validated_data):
+        validated_data['user_id'] = uuid.uuid4()
+        validated_data['password'] = hashlib.sha256().update(validated_data['password'].encode('utf-8').hexdigest())
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.user_id = validated_data.get('user_id',instance.user_id)
+        instance.user_name = validated_data.get('user_name',instance.user_name)
+        instance.password = hashlib.sha256().update(validated_data['password'].encode('utf-8').hexdigest())
+        instance.save()
+        return instance
+

cf7a4cc62215caff08588166ee83156523169344
SamLangTen <samlangten@outlook.com>
2018-02-01 00:05:40 +0800

    add test drift bottle drag

--- PocaExchange/api/tests/functions/drift_bottle_api_test.py
+++ PocaExchange/api/tests/functions/drift_bottle_api_test.py
@@ -21,12 +21,42 @@ class DriftBottleAPITest(APITestCase):
         response = self.client.post('/api/account/login/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
+    def _create_user_2(self):
+        if User.objects.filter(username='pocaetestuser2').exists():
+            test_user = User.objects.get(username='pocaetestuser2')
+            test_user.set_password('password')
+        else:
+            User.objects.create_user(
+                'pocaetestuser2', 'pocaetestuser2@admin.com', 'password')
+
+    def _login_2(self):
+        data = {'username': 'pocaetestuser2', 'password': 'password'}
+        response = self.client.post('/api/account/login/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def _logout(self):
+        response = self.client.post('/api/account/logout/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
     def test_drift_bottle_throw(self):
         self._create_user()
         self._login()
         response = self.client.post('/api/driftbottle/pools/', format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         response = self.client.post('/api/driftbottle/pools/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+
+    def test_drift_bottle_drag(self):
+        self._create_user()
+        self._login()
+        response = self.client.post('/api/driftbottle/pools/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self._logout()
+        self._create_user_2()
+        self._login_2()
+        response = self.client.put('/api/driftbottle/pools/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.put('/api/driftbottle/pools/', format='json')
         self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
 
     def test_drift_bottle_get(self):

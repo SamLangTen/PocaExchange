2efb70705e58d67fc4fd6b739daa314a2850a5d7
SamLangTen <samlangten@outlook.com>
2018-01-31 16:11:06 +0800

    add throw drift bottle api

--- PocaExchange/api/views/drift_bottle.py
+++ PocaExchange/api/views/drift_bottle.py
@@ -1,8 +1,11 @@
 from django.http import Http404
 from django.contrib.auth.models import *
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from api.models import *
 from api.serializers import *
 import uuid
@@ -11,24 +14,25 @@ import datetime
 
 class DriftBottleList(APIView):
 
+    @permission_classes((IsAdminUser, ))
     def get(self, request, format=None):
         bottles = DriftBottle.objects.all()
         serializers = DriftBottleSerializer(bottles, many=True)
         return Response(serializers.data, status=status.HTTP_200_OK)
 
-    def post(self, request, format=None):
-        serializer = DriftBottleSerializer(data=request.data)
-        if serializer.is_valid():
+    @permission_classes((IsAuthenticated, ))
+    def post(self, request, format=None):       
+        if not DriftBottle.objects.filter(request_name=request.user).exists():
             bottle = DriftBottle(bottle_id=uuid.uuid4())
-            bottle.request_name = User.objects.get(
-                username=serializer.validated_data['request_name'])
+            bottle.request_name = request.user
             bottle.throw_time = datetime.datetime.now()
             bottle.save()
             return Response(status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class DriftBottleDetail(APIView):
-    
+
     def post(self, request, format=None):
         pass

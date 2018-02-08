9e1d23cbec4f6afc9535a8af0c0df1686594932e
SamLangTen <samlangten@outlook.com>
2018-02-08 21:54:38 +0800

    fix some name

--- PocaExchange/PocaExchange/settings.py
+++ PocaExchange/PocaExchange/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/dist/'
 # Add for vuejs
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/dist"),

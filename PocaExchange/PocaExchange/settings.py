4ad46c4ed038e897723fe0343c599c876d7a05b1
SamLangTen <samlangten@outlook.com>
2018-02-08 21:53:18 +0800

    enable frontend into django

--- PocaExchange/PocaExchange/settings.py
+++ PocaExchange/PocaExchange/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'PocaExchange.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['frontend'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# Add for vuejs
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/dist"),
+]
